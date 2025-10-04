ab4c9802b7e3de79d0dc04b180330246891934ef
Liam Humphreys <liamjameshumphreys@gmail.com>
2025-10-04 13:21:03 +0100

    Fixed nvim tree signcolumn issue

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,45 +1,35 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	lazy = false,
-	config = function()
-		vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=None]])
-		require("nvim-tree").setup({
-			view = {
-				number = false,
-				relativenumber = false,
-				signcolumn = "no",
-			},
-			filters = {
-				dotfiles = false,
-				git_ignored = false,
-				custom = {
-					".DS_Store",
-					"^.git$",
-					"__pycache__",
-					"^.venv$",
-					".mypy_cache",
-					".ruff_cache",
-					"**egg-info",
-				},
-			},
-		})
-		-- Disable columns in NvimTree window to prevent shifting
-		local group = vim.api.nvim_create_augroup("NvimTreeNoColumns", { clear = true })
-		vim.api.nvim_create_autocmd({ "FileType", "BufWinEnter", "WinEnter" }, {
-			group = group,
-			callback = function(args)
-				local buf = args.buf
-				if vim.bo[buf].filetype == "NvimTree" then
-					local win = vim.fn.bufwinid(buf)
-					if win ~= -1 then
-						vim.wo[win].statuscolumn = ""
-						vim.wo[win].number = false
-						vim.wo[win].relativenumber = false
-						vim.wo[win].signcolumn = "no"
-						vim.wo[win].foldcolumn = "0"
-					end
-				end
-			end,
-		})
-	end,
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  config = function()
+    vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=None]])
+    require("nvim-tree").setup({
+      view = {
+        number = false,
+        relativenumber = false,
+        signcolumn = "no",
+      },
+      filters = {
+        dotfiles = false,
+        git_ignored = false,
+        custom = {
+          ".DS_Store",
+          "^.git$",
+          "__pycache__",
+          "^.venv$",
+          ".mypy_cache",
+          ".ruff_cache",
+          "**egg-info",
+        },
+      },
+    })
+    -- Remove status colum so tree isn't padded with dead space
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+      callback = function()
+        if vim.bo.filetype == "NvimTree" then
+          vim.wo.statuscolumn = ""
+        end
+      end,
+    })
+  end,
 }
