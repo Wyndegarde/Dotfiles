db95814ee6d513533d6621502defa86b7b5980ad
Liam Humphreys <liamjameshumphreys@gmail.com>
2025-10-05 06:55:39 +0100

    fixed toggle term

--- nvim/lua/plugins/toggle-term.lua
+++ nvim/lua/plugins/toggle-term.lua
@@ -12,7 +12,7 @@ return {
     end
     require("toggleterm").setup({
       size = 20,
-      open_mapping = [[<c-p>]],
+      -- open_mapping = [[<c-P>]],
       hide_numbers = true,
       shade_terminals = true,
       start_in_insert = true,
@@ -29,21 +29,33 @@ return {
           background = "Normal",
         },
       },
-      -- Auto-activate venv if present
+    })
+
+    local Python_Terminal = require("toggleterm.terminal").Terminal
+    local python = Python_Terminal:new({
       on_create = function(term)
         local venv_cmd = get_venv_command()
         if venv_cmd ~= "" then
-          -- Send the activation command to the terminal
           vim.api.nvim_chan_send(term.job_id, venv_cmd .. "\n")
         end
       end,
+      hidden = true
     })
+
+    function _python_toggle()
+      python:toggle()
+    end
+
     local Cursor_Terminal = require("toggleterm.terminal").Terminal
     local cursor = Cursor_Terminal:new({ cmd = "cursor-agent", hidden = true })
 
     function _cursor_toggle()
       cursor:toggle()
     end
+
+    vim.api.nvim_set_keymap("n", "<c-p>", "<cmd>lua _python_toggle()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("t", "<c-p>", "<cmd>lua _python_toggle()<CR>", { noremap = true, silent = true })
+    -- vim.api.nvim_set_keymap("i", "<c-p>", "<cmd>lua _python_toggle()<CR>", { noremap = true, silent = true })
 
     vim.api.nvim_set_keymap("n", "<c-g>", "<cmd>lua _cursor_toggle()<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("t", "<c-g>", "<cmd>lua _cursor_toggle()<CR>", { noremap = true, silent = true })
